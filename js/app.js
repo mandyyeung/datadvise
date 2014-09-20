5e8b74ce228fda998268b4ae8c663c2cd21036a9
Mandy Yeung <mandycodes@gmail.com>
2014-09-20 14:51:57 -0400

    Working isotope filter with search option

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(window).scroll(function() {
     }
 });
 
-// Isotope
+// Isotope - filter
 
 $(window).load(function(){
     var $container = $('#resourceContainter');
@@ -27,6 +27,7 @@ $(window).load(function(){
     $('.resourceFilter a').click(function(){
         $('.resourceFilter .current').removeClass('current');
         $(this).addClass('current');
+        $('#quicksearch').val('');
 
         var selector = $(this).attr('data-filter');
         $container.isotope({
@@ -38,5 +39,55 @@ $(window).load(function(){
             }
          });
          return false;
+    });
+
+    $('#quicksearch').focus(function(){
+      $container.isotope({
+          filter: '*',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+      $('.resourceFilter .current').removeClass('current');
+      $('#all-categories').addClass('current');
+
+      // Isotope - search
+
+      $( function() {
+        // quick search regex
+        var qsRegex;
+
+        // init Isotope
+        var $container = $('.resourceContainer').isotope({
+          filter: function() {
+            return qsRegex ? $(this).text().match( qsRegex ) : true;
+          }
+        });
+
+        // use value of search field to filter
+        var $quicksearch = $('#quicksearch').keyup( debounce( function() {
+          qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+          $container.isotope();
+        }, 200 ) );
+
+      });
+
+      // debounce so filtering doesn't happen every millisecond
+      function debounce( fn, threshold ) {
+        var timeout;
+        return function debounced() {
+          if ( timeout ) {
+            clearTimeout( timeout );
+          }
+          function delayed() {
+            fn();
+            timeout = null;
+          }
+          timeout = setTimeout( delayed, threshold || 100 );
+        }
+      }
+
     });
 });
